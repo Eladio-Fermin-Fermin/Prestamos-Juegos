7fff88b1826609c64737a40ac7299b74ea1d7959
ULYSSESS <ULYSSESS@DESKTOP-MK9933E>
2020-08-11 14:29:53 -0400

    clase AmigosBLL lista.

---
+++ DAL/Contexto.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PrestamosJuegos.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PrestamosJuegos.DAL
+{
+    public class Contexto : DbContext
+    {
+        public DbSet<Amigos> Amigos { get; set; }
+        public DbSet<Juegos> Juegos { get; set; }
+        public DbSet<Prestamos> Prestamos { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.EnableSensitiveDataLogging().UseSqlite(@"Data Source= DATA\Juegos.db");
+        }
+
+    }
+}
