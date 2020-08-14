e61c10e7d65487f05fe06612a0daa719e993df1c
ULYSSESS <ULYSSESS@DESKTOP-MK9933E>
2020-08-14 09:04:32 -0400

    Consultas listas Modificando iconos y apariencia de las consultas.

--- UI/Consultas/cEntradas.xaml.cs
+++ UI/Consultas/cEntradas.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using PrestamosJuegos.BLL;
+using PrestamosJuegos.Entidades;
+using System;
 using System.Collections.Generic;
 using System.Text;
 using System.Windows;
@@ -20,6 +22,42 @@ namespace PrestamosJuegos.UI.Consultas
         public cEntradas()
         {
             InitializeComponent();
+        }
+
+        private void ConsultarButton_Click(object sender, RoutedEventArgs e)
+        {
+            var listado = new List<Entradas>();
+
+            if (CriterioTextBox.Text.Trim().Length > 0)
+            {
+                switch (FiltroComboBox.SelectedIndex)
+                {
+                    case 0: //EntradaId
+                        listado = EntradasBLL.GetList(a => a.EntradaId == Utilidades.ToInt(CriterioTextBox.Text));
+                        break;
+
+                    case 1: //JuegoId                       
+                        listado = EntradasBLL.GetList(a => a.JuegoId == Utilidades.ToInt(CriterioTextBox.Text));
+                        break;
+
+                    case 2: //Cantidad                       
+                        listado = EntradasBLL.GetList(a => a.Cantidad == Utilidades.ToInt(CriterioTextBox.Text));
+                        break;
+                }
+            }
+            else
+            {
+                listado = EntradasBLL.GetList(c => true);
+            }
+
+            if (DesdeDataPicker.SelectedDate != null)
+                listado = EntradasBLL.GetList(c => c.FechaEntrada.Date >= DesdeDataPicker.SelectedDate);
+
+            if (HastaDatePicker.SelectedDate != null)
+                listado = EntradasBLL.GetList(c => c.FechaEntrada.Date <= HastaDatePicker.SelectedDate);
+
+            DatosDataGrid.ItemsSource = null;
+            DatosDataGrid.ItemsSource = listado;
         }
     }
 }
--- UI/Consultas/cPrestamos.xaml.cs
+++ UI/Consultas/cPrestamos.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using PrestamosJuegos.BLL;
+using PrestamosJuegos.Entidades;
+using System;
 using System.Collections.Generic;
 using System.Text;
 using System.Windows;
@@ -20,6 +22,50 @@ namespace PrestamosJuegos.UI.Consultas
         public cPrestamos()
         {
             InitializeComponent();
+        }
+
+        private void ConsultarButton_Click(object sender, RoutedEventArgs e)
+        {
+            var listado = new List<Prestamos>();
+
+            if (CriterioTextBox.Text.Trim().Length > 0)
+            {
+                switch (FiltroComboBox.SelectedIndex)
+                {
+                    case 0: //PrestamoId
+                        listado = PrestamosBLL.GetList(p => p.PrestamoId == Utilidades.ToInt(CriterioTextBox.Text));
+                        break;
+
+                    case 1: //JuegoId                       
+                        listado = PrestamosBLL.GetList(p => p.JuegoId == Utilidades.ToInt(CriterioTextBox.Text));
+                        break;
+
+                    /*case 2: //AmigoId                       
+                        listado1 = PrestamosBLL.GetList(p => p.AmigoId == Utilidades.ToInt(CriterioTextBox.Text));
+                        break;*/
+
+                    case 3: //Observacion                       
+                        listado = PrestamosBLL.GetList(p => p.Observacion.Contains(CriterioTextBox.Text, StringComparison.OrdinalIgnoreCase));
+                        break;
+
+                    case 4: //CantidadJuegos                       
+                        listado = PrestamosBLL.GetList(p => p.CantidadJuegos == Utilidades.ToInt(CriterioTextBox.Text));
+                        break; 
+                }
+            }
+            else
+            {
+                listado = PrestamosBLL.GetList(c => true);
+            }
+
+            if (DesdeDataPicker.SelectedDate != null)
+                listado = PrestamosBLL.GetList(c => c.Fecha.Date >= DesdeDataPicker.SelectedDate);
+
+            if (HastaDatePicker.SelectedDate != null)
+                listado = PrestamosBLL.GetList(c => c.Fecha.Date <= HastaDatePicker.SelectedDate);
+
+            DatosDataGrid.ItemsSource = null;
+            DatosDataGrid.ItemsSource = listado;
         }
     }
 }
